17d6b3cd7cadbed748dc7849e397ea77fce6a2e1
Yuyu Qian <Yuyu.Qian.Rice@gmail.com>
2020-12-05 03:20:09 -0600

    comments

--- UUIns/Controller/FeedViewController.swift
+++ UUIns/Controller/FeedViewController.swift
@@ -37,7 +37,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         query.findObjectsInBackground{
             (posts, err) in
@@ -49,21 +49,69 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 1
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Postcell") as! PostTableViewCell
-        
-        let post = posts[indexPath.row]
-        let user = post["author"] as! PFUser
-        cell.nameField.text = user.username
-        cell.captionFeild.text = post["caption"] as! String
-        let img = post["image"] as! PFFileObject
-        let url = URL(string: img.url!)!
-        cell.imageView?.af_setImage(withURL: url)
-        cell.imageView?.contentMode = UIView.ContentMode.scaleAspectFit;
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Postcell") as! PostTableViewCell
+            
+            let post = posts[indexPath.section]
+            let user = post["author"] as! PFUser
+            cell.nameField.text = user.username
+            cell.captionFeild.text = post["caption"] as! String
+            let img = post["image"] as! PFFileObject
+            let url = URL(string: img.url!)!
+            cell.imageView?.af_setImage(withURL: url)
+            cell.imageView?.contentMode = UIView.ContentMode.scaleAspectFit;
 
-        return cell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as! String
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username as! String
+            return cell
+        }
+
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.section]
+
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey:"comments")
+        post.saveInBackground {
+            (success, error) in
+            if success {
+                print("comment saved")
+            } else {
+                print("Error saving comment: \(error)")
+            }
+        }
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let sceneDelegate = view.window?.windowScene?.delegate as! SceneDelegate
+        sceneDelegate.window?.rootViewController = loginViewController
+    }
+    
 }
